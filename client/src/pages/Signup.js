aa40197e332d178ca6aee7160c2859e4949edcce
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-12-11 14:20:39 -0500

    styled SignUp

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
+import image from '../assets/images/BurfordTogether.jpeg'
 
-const Signup = () => {
-  const [formState, setFormState] = useState({ username: '', email: '', password: '' });
-  const [addUser, { error }] = useMutation(ADD_USER);
+export default function Signup() {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -18,70 +20,60 @@ const Signup = () => {
   };
 
   // submit form (notice the async!)
-const handleFormSubmit = async event => {
-  event.preventDefault();
+  const handleFormSubmit = async event => {
+    event.preventDefault();
 
-  // use try/catch instead of promises to handle errors
-  try {
-    // execute addUser mutation and pass in variable data from form
-    const { data } = await addUser({
-      variables: { ...formState }
-    });
-    
-    Auth.login(data.addUser.token);
-  } catch (e) {
-    console.error(e);
-  }
-};
+    // use try/catch instead of promises to handle errors
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
-    <main className='flex-row justify-center mb-4'>
-      <div className='col-12 col-md-6'>
-        {/* Card */}
-        <div className='card'>
-          {/* Card Header */}
-          <h4 className='card-header'>Sign Up</h4>
-          {/* Card Body */}
-          <div className='card-body'>
-            {/* Form */}
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className='form-input'
-                placeholder='Your username'
-                name='username'
-                type='username'
-                id='username'
-                value={formState.username}
-                onChange={handleChange}
-              />
-              <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className='btn d-block w-100' type='submit'>
-                Submit
-              </button>
+    // Card for signup
+    <section className="flex flex-col items-center justify-center px-6 py-8 pt-[60px] mx-auto md:h-screen lg:py-0 text-stone-800">
+      <h1 className="flex items-center mb-6 text-3xl border-b-4 border-pcCoral">
+        Create Your Account
+      </h1>
+      <div
+        className="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0"
+      >
+        {/* Left Side */}
+        {/* Card interior placement */}
+        <div className="form-container">
+          <div className="form-inner-container">
+            <h1 className="items-center justify-center">
+              Please provide an email and password
+            </h1>
+            {/* Form for input - contains submit button */}
+            <form className="space-y-4 md:space-y-6" onSubmit={handleFormSubmit}>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium ">Your email</label>
+                <input type="email" name="email" id="email" className="form-field  mb-4 focus: outline-pcGreen" placeholder="name@email.com" onChange={handleChange} />
+              </div>
+              {/* password input */}
+              <div>
+                <label for="password" className="block mb-2 text-sm font-medium ">Password</label>
+                <input type="password" name="password" placeholder="••••••••" className="form-field  mb-4 focus: outline-pcGreen" onChange={handleChange} />
+              </div>
+              <div className="flex items-center justify-evenly space-x-2">
+                {/* submit button */}
+                <button type="submit" className="form-button">Submit</button>
+                <button type="submit" className="form-button"><Link to="/login">Login?</Link></button>
+              </div>
             </form>
-            {error && <div>Sign up failed</div>}
           </div>
         </div>
+        {/* Right Side */}
+        <img src={image} alt="" className="w-[430px] hidden md:block" />
       </div>
-    </main>
+    </section>
   );
-};
-
-export default Signup;
+}
